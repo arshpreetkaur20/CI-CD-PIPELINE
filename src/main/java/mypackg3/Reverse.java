2a95aaa88f6e0ed4916be25d0f48100375dae7c7
Arshpreet Kaur <arshpreetk498@gmail.com>
2024-03-17 21:27:08 +0530

    Main function added to reverse the numbers

--- src/main/java/mypackg3/Reverse.java
+++ src/main/java/mypackg3/Reverse.java
@@ -1,20 +1,30 @@
 package mypackg3;
+
 public class Reverse {
-public int reverse(int num) {
-boolean isNoNegative = num < 0 ? true : false; 
-if(isNoNegative) 
-{ 
-num= num * -1; //makes the number positive if the given number is negative 
-} 
-int reverse = 0; 
-int lastDigit = 0; 
-while (num>= 1) 
-{ 
-lastDigit = num % 10; // gives the last digit of the number 
-reverse = reverse * 10 + lastDigit; 
-num = num/ 10; // removes the last digit of the number 
-} 
-//makes the number negative 
-return isNoNegative == true? reverse*-1 : reverse; 
-}
+    public int reverse(int num) {
+        boolean isNoNegative = num < 0 ? true : false; 
+        if (isNoNegative) { 
+            num = num * -1; // makes the number positive if the given number is negative 
+        } 
+        int reverse = 0; 
+        int lastDigit = 0; 
+        while (num >= 1) { 
+            lastDigit = num % 10; // gives the last digit of the number 
+            reverse = reverse * 10 + lastDigit; 
+            num = num / 10; // removes the last digit of the number 
+        } 
+        // makes the number negative 
+        return isNoNegative ? reverse * -1 : reverse; 
+    }
+    
+    public static void main(String[] args) {
+        Reverse reverseObj = new Reverse();
+        
+        // Test cases
+        int num1 = 12345;
+        int num2 = -54321;
+        
+        System.out.println("Reverse of " + num1 + " is: " + reverseObj.reverse(num1));
+        System.out.println("Reverse of " + num2 + " is: " + reverseObj.reverse(num2));
+    }
 }
