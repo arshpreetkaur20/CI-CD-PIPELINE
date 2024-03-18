1051c2bf3a2815ccbb72fab05b5f9d275be100b4
pragya2003 <500090986@stu.upes.ac.in>
2024-03-18 10:44:59 +0530

    To find Prime numbers between 1-100

---
+++ src/main/java/mypackg4/Prime_Num.java
@@ -0,0 +1,31 @@
+package mypackg4;
+
+public class Prime_Num {
+	
+	
+	public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+	
+	public static void findPrimes(int limit) {
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+	
+	public static void main(String[] args) {
+        int n = 100; // Change this to find prime numbers up to a different limit
+        System.out.println("Prime numbers up to " + n + ":");
+        findPrimes(n);
+    }
+}
